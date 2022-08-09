2c157b7a3487cc11036129ba647e66ab62e0226f
Berezyak Vadim <newberez@gmail.com>
2022-08-09 17:43:55 +0300

    Second part in of homework is done

--- src/test/java/com/heroku/HerokuAppTest.java
+++ src/test/java/com/heroku/HerokuAppTest.java
@@ -4,27 +4,41 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.interactions.Actions;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class HerokuAppTest {
     @BeforeAll
     static void configuration() {
 //      Configuration.headless = true;
-        Configuration.browser = "firefox";
+//      Configuration.browser = "firefox";
         Configuration.browserSize = "1024x768";
     }
-    @Test
-    void dragAndDropTest(){
+    @BeforeEach
+    void beforeEachTEst(){
         open("https://the-internet.herokuapp.com/drag_and_drop");
         Selenide.clearBrowserCookies();
         Selenide.clearBrowserLocalStorage();
         executeJavaScript("sessionStorage.clear();");
+    }
+    @Test
+    void dragAndDropTest(){
+        $("#column-a").shouldHave(text("a"));
+        $("#column-b").shouldHave(text("b"));
         $("#column-a").dragAndDropTo($("#column-b"));
-        $("#column-a").shouldHave(Condition.text("B"));
-        $("#column-b").shouldHave(Condition.text("A"));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
 
+    }
+    @Test
+    void actionsDragAndDropTest(){
+        $("#column-a").shouldHave(text("a"));
+        $("#column-b").shouldHave(text("b"));
+//        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(30, 0).release().perform();
+//        actions().dragAndDropBy($("#column-b"), 250, 0).perform();  todo: выяснить почему не работает
     }
 }
