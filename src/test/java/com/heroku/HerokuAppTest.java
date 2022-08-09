5338e444cd13ba522651e0e254bcacdd7a9d5096
Berezyak Vadim <newberez@gmail.com>
2022-08-09 17:07:30 +0300

    Second part in of homework is in progress

--- src/test/java/com/heroku/HerokuAppTest.java
+++ src/test/java/com/heroku/HerokuAppTest.java
@@ -1,31 +1,30 @@
-package com.github;
+package com.heroku;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.interactions.Actions;
 
-import static com.codeborne.selenide.CollectionCondition.*;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class GitHubTest {
+public class HerokuAppTest {
     @BeforeAll
     static void configuration() {
 //      Configuration.headless = true;
         Configuration.browser = "firefox";
         Configuration.browserSize = "1024x768";
     }
-
     @Test
-    void someGithubTest() {
-        open("https://github.com");
+    void dragAndDropTest(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
         Selenide.clearBrowserCookies();
         Selenide.clearBrowserLocalStorage();
         executeJavaScript("sessionStorage.clear();");
-        $(byText("Pricing")).hover();
-        $("div.dropdown-menu a[href='/pricing#compare-features']").click();
-        $$("h2.h3.mt-1").shouldHave(textsInAnyOrder("Free", "Team", "Enterprise"));
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-a").shouldHave(Condition.text("B"));
+        $("#column-b").shouldHave(Condition.text("A"));
+
     }
 }
